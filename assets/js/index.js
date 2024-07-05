eb79d490138756f4d46a3cede60c6065f2cb22ad
Vathender <bvathender64@gmail.com>
2024-07-05 10:09:09 +0530

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth , googleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDz2kvMDwx6fHRj2tkzWACLE6wyxICAshs",
+  authDomain: "limitless-login-2db57.firebaseapp.com",
+  projectId: "limitless-login-2db57",
+  storageBucket: "limitless-login-2db57.appspot.com",
+  messagingSenderId: "306658783455",
+  appId: "1:306658783455:web:75431e75a32ce069ee796a",
+  measurementId: "G-XZHKZP2J7H"
+};
+
+
